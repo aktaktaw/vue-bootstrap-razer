a38c77bac34282bc37ffc5ed3d47ec443181f429
aktaktaw <aktaktaw@gmail.com>
2020-03-05 20:28:01 +0800

    add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            </b-nav-form>
+
+            <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item>Documentations</b-nav-item>
+            <b-nav-item>Guides</b-nav-item>
+            <b-nav-item>Contributes</b-nav-item>
+            <b-button size="sm" class="my-4 my-sm-2" variant="danger" to="/login">Sign Up</b-button>
+        </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+    </div>
+</template>
+
+<style scoped>
+ #navbar{
+     padding-left: 30px;
+     padding-right: 30px;
+ }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/Home')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('@/views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/Login.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
